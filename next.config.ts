e3483270e5e60ef1614d6c991ff619e3c6b9af05
Pied07 <sandipan@swachalit.co.in>
2026-09-22 17:05:23 +0530

    parser error

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,8 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   allowedDevOrigins: ["192.168.0.194"],
   serverExternalPackages: ["pdf-parse", "pdfjs-dist", "tesseract.js"],
-  experimental: {
-    outputFileTracingIncludes: {
-      "/api/resume/parse": ["./node_modules/pdfjs-dist/**/*", "./node_modules/tesseract.js/**/*"],
-    },
+  outputFileTracingIncludes: {
+    "/api/resume/parse": ["./node_modules/pdfjs-dist/**/*", "./node_modules/tesseract.js/**/*"],
   },
 };
 
